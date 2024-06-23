c41d9166e5b73ee10cb0d9c4474e06277fae945f
EmirAKSOY1 <emiraksoy10@gmail.com>
2024-06-23 15:04:31 +0300

    add newmigration

---
+++ database/migrations/2024_06_23_115055_add_new_column_to_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Yeni sütunu eklemek için Blueprint kullanarak 'new_column' adında bir sütun ekleyelim
+            $table->string('tc')->after('email')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('table', function (Blueprint $table) {
+            //
+        });
+    }
+};
